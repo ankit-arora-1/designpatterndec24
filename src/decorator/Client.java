4eb4a64fab043faa05f51189b3d4a4a5f49a920b
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2025-01-07 09:12:19 +0530

    Added observer and decorator

---
+++ src/decorator/Client.java
@@ -0,0 +1,24 @@
+package decorator;
+
+public class Client {
+
+    public static void main(String[] args) {
+        IceCream iceCream =
+                new ChocoChips(
+                        new VanillaScoop(
+                                new StrawberryScoop(
+                                        new ChocoChips(
+                                                new ChocolateCone(
+                                                        new ChocolateCone(
+                                                                new OrangeCone()
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                );
+
+        System.out.println(iceCream.getDescription());
+        System.out.println(iceCream.getCost());
+    }
+}
